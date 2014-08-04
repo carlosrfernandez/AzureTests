2b3d7cb6d78f3741560264e9ef0a21ebe944a7d4
Carlos Fernandez <carlos@allismymusic.org>
2014-08-04 19:21:39 +0200

    Git ignored added + Initial checkin
    Stream errors need to be fixed.

---
+++ SmallTCPServer/MessageServerRole/WorkerRole.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace MessageServerRole
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private readonly AutoResetEvent _connectionWaitHandle = new AutoResetEvent(false);
+
+        public override void Run()
+        {
+            // This is a sample worker implementation. Replace with your logic.
+            Trace.TraceInformation("MessageServerRole entry point called");
+            TcpListener listener;
+
+            try
+            {
+                listener = new TcpListener(RoleEnvironment.CurrentRoleInstance.InstanceEndpoints["DefaultEndpoint"].IPEndpoint.Address, 
+                    RoleEnvironment.CurrentRoleInstance.InstanceEndpoints["DefaultEndpoint"].IPEndpoint.Port)
+                {
+                    ExclusiveAddressUse = false
+                };
+                listener.Start();
+
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                return;
+            }
+            while (true)
+            {
+                var result = listener.BeginAcceptTcpClient(HandleNewConnection, listener);
+                _connectionWaitHandle.WaitOne();
+                Trace.TraceInformation("Working");
+            }
+        }
+
+        private void HandleNewConnection(IAsyncResult ar)
+        {
+            var listener = (TcpListener) ar.AsyncState;
+            var client = listener.EndAcceptTcpClient(ar);
+            _connectionWaitHandle.Set();
+
+            var clientGuid = Guid.NewGuid();
+            Trace.WriteLine(string.Format("Accepted connection with ID {0}", clientGuid));
+
+            var stream = client.GetStream();
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream) { AutoFlush = true };
+
+            var input = string.Empty;
+            while (input != "9")
+            {
+                writer.WriteLineAsync("...");
+
+                input = reader.ReadLine();
+                writer.WriteLine();
+
+                writer.WriteLineAsync(string.Format("Received: {0}", input));
+                writer.WriteLine();
+            }
+            stream.Close();
+            reader.Close();
+            writer.Close();
+            client.Close();
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections 
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // For information on handling configuration changes
+            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+
+            return base.OnStart();
+        }
+    }
+}
